cb767efbf42cdb601e83cd070415dabe70760f11
carlkuesters <carl.kuesters@youengineering.com>
2021-02-02 22:24:15 +0100

    Add initial deploy script.

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+VERSION=$1
+SERVER=$2
+CLIENT=$3
+
+# Checkout
+git clone https://github.com/destrostudios/grid.git
+if [ -n "$VERSION" ]; then
+  git checkout "$VERSION"
+fi
+
+# Build
+mkdir workspace
+echo "../assets/" > workspace/assets.ini
+mvn clean install
+
+# Deploy (Client)
+rm -rf "${CLIENT}"*
+mv assets "${CLIENT}"
+mv grid-client/target/libs "${CLIENT}"
+mv grid-client/target/grid-client-application-0.0.1.jar "${CLIENT}Grid.jar"
+echo "./assets/" > "${CLIENT}assets.ini"
+curl https://destrostudios.com:8080/apps/6/updateFiles
+
+# Deploy (Server)
+mv grid-server/target/grid-server-0.0.1-jar-with-dependencies.jar "${SERVER}grid.jar"
+sh "${SERVER}control.sh" restart
