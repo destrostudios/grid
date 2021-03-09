064b795b15a4a25091e5997c753481aab0eb2d30
Gerd <gerdemmanuel@web.de>
2021-03-09 18:19:04 +0100

    changing import

--- grid-game/src/test/java/com/destrostudios/grid/util/RangeUtilsTest.java
+++ grid-game/src/test/java/com/destrostudios/grid/util/RangeUtilsTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.params.provider.ArgumentsSource;
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RangeUtilsTest {
 
