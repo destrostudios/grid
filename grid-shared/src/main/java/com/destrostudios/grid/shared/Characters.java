ebc51998384313a4d85bed5ca35c7d4b38044cf4
etherblood <philipp@jobach.de>
2021-03-12 20:16:30 +0100

    removed deleted characters from character list

--- grid-shared/src/main/java/com/destrostudios/grid/shared/Characters.java
+++ grid-shared/src/main/java/com/destrostudios/grid/shared/Characters.java
@@ -3,7 +3,7 @@ package com.destrostudios.grid.shared;
 public class Characters {
 
     public static String[] CHARACTER_NAMES = new String[]{
-            "aland", "alice", "dosaz", "dwarf_warrior", "elven_archer", "garmon", "scarlet", "tristan", "iop", "cra"
+            "iop", "cra"
     };
 
     public static String getRandomCharacterName() {
