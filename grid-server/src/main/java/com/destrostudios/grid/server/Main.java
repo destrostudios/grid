75f62f7631bc7c1806cccfd9321ee70b7c274e1b
etherblood <philipp@jobach.de>
2021-10-15 13:11:50 +0200

    split server into classes

--- grid-server/src/main/java/com/destrostudios/grid/server/Main.java
+++ grid-server/src/main/java/com/destrostudios/grid/server/Main.java
@@ -1,34 +1,27 @@
 package com.destrostudios.grid.server;
 
-import com.destrostudios.authtoken.JwtAuthenticationUser;
 import com.destrostudios.authtoken.JwtService;
 import com.destrostudios.authtoken.NoValidateJwtService;
 import com.destrostudios.grid.GridGame;
 import com.destrostudios.grid.actions.Action;
 import com.destrostudios.grid.network.KryoStartGameInfo;
 import com.destrostudios.grid.network.NetworkGridService;
-import com.destrostudios.grid.random.MutableRandomProxy;
+import com.destrostudios.grid.server.modules.AutoRejoinModule;
+import com.destrostudios.grid.server.modules.GameOverModule;
+import com.destrostudios.grid.server.modules.GridStartModule;
+import com.destrostudios.grid.server.modules.LogStateHashModule;
 import com.destrostudios.grid.shared.StartGameInfo;
-import com.destrostudios.grid.util.GameOverInfo;
 import com.destrostudios.turnbasedgametools.network.server.ToolsServer;
 import com.destrostudios.turnbasedgametools.network.server.modules.game.GameServerModule;
 import com.destrostudios.turnbasedgametools.network.server.modules.game.GameStartServerModule;
 import com.destrostudios.turnbasedgametools.network.server.modules.game.LobbyServerModule;
-import com.destrostudios.turnbasedgametools.network.server.modules.game.ServerGameData;
 import com.destrostudios.turnbasedgametools.network.server.modules.jwt.JwtServerModule;
 import com.destrostudios.turnbasedgametools.network.shared.NetworkUtil;
 import com.destrostudios.turnbasedgametools.network.shared.modules.NetworkModule;
-import com.destrostudios.turnbasedgametools.network.shared.modules.game.messages.GameActionRequest;
-import com.destrostudios.turnbasedgametools.network.shared.modules.jwt.messages.Login;
-import com.esotericsoftware.kryonet.Connection;
 import com.esotericsoftware.kryonet.Server;
 import com.esotericsoftware.minlog.Log;
 import java.io.IOException;
-import java.security.SecureRandom;
 import java.util.Date;
-import java.util.Map;
-import java.util.UUID;
-import java.util.stream.Stream;
 
 public class Main {
 
@@ -45,75 +38,14 @@ public class Main {
         JwtServerModule jwtModule = new JwtServerModule(jwtService, kryoServer::getConnections);
         GameServerModule<GridGame, Action> gameModule = new GameServerModule<>(gameService, kryoServer::getConnections);
         LobbyServerModule<StartGameInfo> lobbyModule = new LobbyServerModule<>(KryoStartGameInfo::initialize, kryoServer::getConnections);
-        GameStartServerModule<StartGameInfo> gameStartModule = new GameStartServerModule<>(KryoStartGameInfo::initialize) {
-            @Override
-            public void startGameRequest(Connection connection, StartGameInfo startGameInfo) {
-                UUID gameId = UUID.randomUUID();
-                GridGame gridGame = new GridGame(new MutableRandomProxy(new SecureRandom()::nextInt));
-                gridGame.initGame(startGameInfo);
-
-                lobbyModule.listGame(gameId, startGameInfo);
-                gameModule.registerGame(new ServerGameData<>(gameId, gridGame, new SecureRandom()));
-
-                for (Connection other : kryoServer.getConnections()) {
-                    JwtAuthenticationUser user = jwtModule.getUser(other.getID());
-                    if (user != null && isUserInGame(startGameInfo, user)) {
-                        gameModule.join(other, gameId);
-                    }
-                }
-            }
-        };
-        NetworkModule gameOverModule = new NetworkModule() {
-            @Override
-            public void received(Connection connection, Object object) {
-                if (object instanceof GameActionRequest) {
-                    GameActionRequest message = (GameActionRequest) object;
-                    ServerGameData<GridGame> game = gameModule.getGame(message.game);
-
-                    GameOverInfo gameOverInfo = game.state.getGameOverInfo();
-                    if (gameOverInfo.isGameIsOver()) {
-                        lobbyModule.unlistGame(game.id);
-                        gameModule.unregisterGame(game.id);
-                    }
-                }
-            }
-        };
-        NetworkModule autoRejoinModule = new NetworkModule() {
-            @Override
-            public void received(Connection connection, Object object) {
-                if (object instanceof Login) {
-                    JwtAuthenticationUser user = jwtModule.getUser(connection.getID());
-                    if (user == null) {
-                        // unsuccessful login
-                        return;
-                    }
-                    for (Map.Entry<UUID, StartGameInfo> entry : lobbyModule.getGames().entrySet()) {
-                        StartGameInfo startGameInfo = entry.getValue();
-                        if (isUserInGame(startGameInfo, user)) {
-                            gameModule.join(connection, entry.getKey());
-                        }
-                    }
-                }
-            }
-        };
-        NetworkModule logStateHashModule = new NetworkModule() {
-            @Override
-            public void received(Connection connection, Object object) {
-                if (object instanceof GameActionRequest) {
-                    GameActionRequest message = (GameActionRequest) object;
-                    System.out.println("Game state hash before action: " + Integer.toHexString(gameModule.getGame(message.game).state.getState().hashCode()));
-                }
-            }
-        };
+        GameStartServerModule<StartGameInfo> gameStartModule = new GridStartModule(KryoStartGameInfo::initialize, kryoServer, jwtModule, gameModule, lobbyModule);
+        NetworkModule gameOverModule = new GameOverModule(lobbyModule, gameModule);
+        NetworkModule autoRejoinModule = new AutoRejoinModule(jwtModule, lobbyModule, gameModule);
+        NetworkModule logStateHashModule = new LogStateHashModule(gameModule);
 
         ToolsServer server = new ToolsServer(kryoServer, logStateHashModule, jwtModule, gameModule, lobbyModule, gameStartModule, gameOverModule, autoRejoinModule);
         server.start(NetworkUtil.PORT);
 
         System.out.println("Server started.");
-    }
-
-    private static boolean isUserInGame(StartGameInfo startGameInfo, JwtAuthenticationUser user) {
-        return Stream.concat(startGameInfo.getTeam1().stream(), startGameInfo.getTeam2().stream())
-                .anyMatch(p -> p.getId() == user.id);
     }
 }
---
+++ grid-server/src/main/java/com/destrostudios/grid/server/modules/LogStateHashModule.java
@@ -0,0 +1,25 @@
+package com.destrostudios.grid.server.modules;
+
+import com.destrostudios.grid.GridGame;
+import com.destrostudios.grid.actions.Action;
+import com.destrostudios.turnbasedgametools.network.server.modules.game.GameServerModule;
+import com.destrostudios.turnbasedgametools.network.shared.modules.NetworkModule;
+import com.destrostudios.turnbasedgametools.network.shared.modules.game.messages.GameActionRequest;
+import com.esotericsoftware.kryonet.Connection;
+
+public class LogStateHashModule extends NetworkModule {
+
+    private final GameServerModule<GridGame, Action> gameModule;
+
+    public LogStateHashModule(GameServerModule<GridGame, Action> gameModule) {
+        this.gameModule = gameModule;
+    }
+
+    @Override
+    public void received(Connection connection, Object object) {
+        if (object instanceof GameActionRequest) {
+            GameActionRequest message = (GameActionRequest) object;
+            System.out.println("Game state hash before action: " + Integer.toHexString(gameModule.getGame(message.game).state.getState().hashCode()));
+        }
+    }
+}
